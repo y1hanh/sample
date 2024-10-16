42f64269cd31591a7fe7c2953016798ad03d8ff5
Yihan Huang <huangehan@gmail.com>
2024-10-16 21:06:49 +1100

    meta.env

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL ? import.meta.env.BASE_URL: "https://y1hanh.github.io/sample/"),
   routes: [
     {
       path: '/',
