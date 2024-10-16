3d2a7aa32d7b042194efd0f733e0f970cfcaca7a
Yihan Huang <huangehan@gmail.com>
2024-10-16 21:22:21 +1100

    web history change

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL ? process.env.BASE_URL : "https://y1hanh.github.io/sample"),
+  history: createWebHistory("/sample/"),
   routes: [
     {
       path: '/',
